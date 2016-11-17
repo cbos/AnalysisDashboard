43a41ef4f19053bfd5428c53c4bd22024c8b91f6
jhaverhals <johanhaverhals@gmail.com>
2016-11-17 12:02:32 +0100

    Revert "Remove commented-out part of JSON output filter"
    This reverts commit f03348fdbe171fc6e96ea3f79f1002fa0cf746d7.

--- app/jsonhandling/RunParser.java
+++ app/jsonhandling/RunParser.java
@@ -1,7 +1,5 @@
 package jsonhandling;
 
-import java.io.UnsupportedEncodingException;
-
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class RunParser extends BaseParser
@@ -102,7 +100,7 @@ public class RunParser extends BaseParser
 		}
 		String filterForJSONOutput = "";
 		// Commented out below lines because not all test-failure related data is retrieved using below filter
-		
+		/*
 		// Below filterForJSONOutput is added because 'stdout' in 'suites[cases[stdout]]]' can blow up returned JSON response above 1GB but it is never used
 		String suitesSelector = "suites[duration,id,name,timestamp,cases[age,className,duration,errorDetails,errorStackTrace,failedSince,name,skipped,status]]";
 		String testReportSelector = "*,childReports[child[number,url],result[*,"+suitesSelector+"]],"+suitesSelector;
@@ -111,7 +109,7 @@ public class RunParser extends BaseParser
     	filterForJSONOutput = "/api/json?tree=" + java.net.URLEncoder.encode(testReportSelector,"UTF-8");
     } catch (UnsupportedEncodingException e) {
 	    e.printStackTrace();
-    }
+    }*/
 		return new TestReportParser(reader.getJSonResult(getUrl() + "testReport" + filterForJSONOutput));
 	}
 }
